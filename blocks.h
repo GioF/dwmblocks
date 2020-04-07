be616d863c386ed11f939f0acc0baf5595ab5121
Giovanny Lucas <giovanny.lucas.f@gmail.com>
2020-04-07 18:54:53 -0300

    trying to solve unknown signal bug

--- blocks.h
+++ blocks.h
@@ -1,19 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
+	{"", "apt list --upgradable | sed 1d | wc -l",					90,		0},
 	
-	{"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "~/bin/statusbar/volume",						0,		10},
-
-	{"☀", "xbacklight | sed 's/\\..*//'",					0,		11},
-	
-	{"", "~/bin/statusbar/battery",						5,		0},
+	{"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	4,		0},
 
 	{"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
 
-	{"", "~/bin/statusbar/clock",						5,		0},
+	{"", "date +\"%a %d/%m/%y %H:%M:%S\"",						1,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
