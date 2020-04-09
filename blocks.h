e61874091e01925ca01d77bd7a316a14aa417310
Giovanny Lucas <giovanny.lucas.f@gmail.com>
2020-04-08 23:16:58 -0300

    formatting

--- blocks.h
+++ blocks.h
@@ -1,15 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"勒", "apt list --upgradable 2>/dev/null | sed 1d | wc -l",					600,		0},
+	{"勒", "apt list --upgradable 2>/dev/null | sed 1d | wc -l",					          600,		0},
 
-  {" ", "unreadmail 2>/dev/null",                                                          2,    0},
+  {" ", "unreadmail 2>/dev/null",                                                  2,    0},
 	
-	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	            4,		0},
+	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	                4,		0},
 
-	{"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
+	{"🌡", "sensors | awk '/^temp1:/{print $2}'" 			                                5,		0},
 
-	{" ", "date +\"%d/%m/%y %H:%M:%S\"",						1,		0},
+	{" ", "date +\"%d/%m/%y %H:%M:%S\"",						                                  1,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
